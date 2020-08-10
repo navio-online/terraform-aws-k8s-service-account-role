5a1a807b2ba55469750bf4f7650554c074e1b903
Peter Salnikov <peter@salnikov.cc>
2020-08-10 17:04:23 +0300

    use namespace-serviceaccount name for role

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_role" "this" {
-  name               = "k8s-${var.k8s_serviceaccount}"
+  name               = "k8s-${var.k8s_namespace}-${var.k8s_serviceaccount}"
   assume_role_policy = data.aws_iam_policy_document.assume_policy.json
 }
 
@@ -24,17 +24,18 @@ data "aws_iam_policy_document" "assume_policy" {
 }
 
 resource "aws_iam_policy" "this" {
-  count  = var.policy_json != "none" && var.policy_arn == "none" ? 1 : 0
+  count  = (var.policy_json != "none" && var.policy_arn == "none") ? 1 : 0
   policy = var.policy_json
 }
 
 resource "aws_iam_role_policy_attachment" "via_json" {
+  count      = length(aws_iam_policy.this)
   role       = aws_iam_role.this.name
-  policy_arn = aws_iam_policy.this[0].arn
+  policy_arn = aws_iam_policy.this[count.index].arn
 }
 
 resource "aws_iam_role_policy_attachment" "via_arn" {
-  count      = var.policy_arn != "none" && var.policy_json == "none" ? 1 : 0
+  count      = (var.policy_arn != "none" && var.policy_json == "none") ? 1 : 0
   role       = aws_iam_role.this.name
   policy_arn = var.policy_arn
 }
